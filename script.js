1ee58ecb78fd3083cf5df8188b0f0a35f011a3ab
niksa <niksaopfor@gmail.com>
2022-03-11 20:04:28 +0100

    Adding

--- script.js
+++ script.js
@@ -3,53 +3,94 @@ const grid = document.querySelector('.grid');
 const boxes = document.querySelectorAll('.box');
 const playerScore0 = document.querySelector('.player_score--0');
 const playerScore1 = document.querySelector('.player_score--1');
+const message = document.querySelector('.message');
 
-// Displaying X and O on the click
-
-const winningConditions = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+// Every winning condition
+let winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 9],
-  [1, 2, 3],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
+let boxIndexValue = new Array(9).fill('');
+console.log(boxIndexValue);
 let activePlayer = 'X';
 
 const player1 = 'X';
 const player2 = 'O';
 
-let player1Elements = [];
-let player2Elements = [];
+let player1Score = 0;
+let player2Score = 0;
 
-// Function swap the player
+let playing = true;
+
+// Function switch the player
 const switchingPlayer = function () {
   activePlayer = activePlayer === 'X' ? 'O' : 'X';
+  message.textContent = `${activePlayer}-turns`;
 };
 
-boxes.forEach(box => {
-  box.addEventListener('click', function (e) {
-    if (activePlayer === player1) {
-      box.textContent = player1;
+const winnerMessage = () => alert(`${activePlayer} won`);
 
-      let id1 = box.getAttribute('id');
-      player1Elements.push(id1);
+const checkWhoWon = function () {
+  if (activePlayer == player1) {
+    player1Score++;
+  } else {
+    player2Score++;
+  }
+};
 
-      console.log(player1Elements);
-      switchingPlayer();
-    } else {
-      box.textContent = player2;
-      let id2 = box.getAttribute('id');
-      console.log(id2);
-      switchingPlayer();
+const checkForWinner = function () {
+  if (playing) {
+    for (let i = 0; i <= 7; i++) {
+      const winningCondition = winningConditions[i];
+      if (
+        boxIndexValue[winningCondition[0]] == '' ||
+        boxIndexValue[winningCondition[1]] == '' ||
+        boxIndexValue[winningCondition[2]] == ''
+      ) {
+        continue;
+      }
+      if (
+        boxIndexValue[winningCondition[0]] ==
+          boxIndexValue[winningCondition[1]] &&
+        boxIndexValue[winningCondition[1]] == boxIndexValue[winningCondition[2]]
+      ) {
+        // winner message
+        winnerMessage();
+        checkWhoWon();
+        playing = false;
+        break;
+      }
     }
 
-    let id = box.getAttribute('id');
+    if (!boxIndexValue.includes('') && playing) {
+      alert('draw');
+    }
+  }
+};
+
+// Displaying X and O on the click
+boxes.forEach(box => {
+  box.addEventListener('click', function (e) {
+    let target = e.target;
+    if (playing) {
+      let boxIndex = box.getAttribute('id');
+
+      if (!playing || boxIndexValue[boxIndex] != '') {
+        return;
+      } else {
+        boxIndexValue[boxIndex] = activePlayer;
+        target.innerHTML = activePlayer;
+        checkForWinner();
+        switchingPlayer();
+      }
+    }
   });
 });
-
-//
+console.log(player1Score);
